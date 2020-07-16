2a850932246729da6960ce0838d348188234555d
qyz96 <adncat@armstrong-login-0-2.localdomain>
2020-07-16 15:26:53 -0700

    checking results

--- run_cholesky.sh
+++ run_cholesky.sh
@@ -4,8 +4,6 @@
 
 hostname
 lscpu
-ml intel/python/2019-3.6.8
-ml gnu8/8.3.0
 
 mpirun -n ${SLURM_NTASKS} hostname
 
@@ -15,4 +13,4 @@ export LD_LIBRARY_PATH="$(pwd):$LD_LIBRARY_PATH"
 echo ">>>>slurm_id=${SLURM_JOB_ID},matrix_size=${MATRIX_SIZE},num_blocks=${NUM_BLOCKS},block_size=${BLOCK_SIZE},num_ranks=${SLURM_NTASKS},num_cpus=${NUM_CPUS}"
 
 # LAUNCHER="mpirun -n ${SLURM_NTASKS}" ./regent.py exam#ples/my_cholesky.rg -fflow 0 -level 5 -n ${MATRIX_SIZE} -p ${NUM_BLOCKS} -ll:csize 16384 -foverride-demand-index-launch 1 -ll:cpu ${NUM_CPUS} -ll:util ${NUM_CPUS} -lg:prof ${SLURM_NTASKS} -lg:prof_logfile prof_${SLURM_JOB_ID}_%.gz
-LAUNCHER="mpirun -n ${SLURM_NTASKS}" ../legion/language/regent.py ./my_cholesky.rg -fflow 0 -level 5 -n ${MATRIX_SIZE} -p ${NUM_BLOCKS} -ll:csize 16384 -foverride-demand-index-launch 1 -ll:cpu ${NUM_CPUS} -ll:util ${NUM_CPUS}
+LAUNCHER="mpirun -n ${SLURM_NTASKS}" ~/legion/language/regent.py ./my_cholesky.rg -fflow 0 -level 5 -n ${MATRIX_SIZE} -p ${NUM_BLOCKS} -ll:csize 16384 -foverride-demand-index-launch 1 -ll:cpu ${NUM_CPUS} -ll:util ${NUM_CPUS}
